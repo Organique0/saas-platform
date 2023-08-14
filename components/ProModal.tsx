3e7a9b634d212481db29442ee4e9962012ce4044
Organique0 <grabnar.luka@gmail.com>
2023-08-14 02:34:25 +0200

    Stripe subscription part 1

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -8,9 +8,25 @@ import { LuCheck, LuCode, LuImage, LuMessageSquare, LuMusic, LuVideo, LuZap } fr
 import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const ProModal = () => {
     const proModal = useProModal();
+    const [loading, setLoading] = useState(false);
+
+    const onSubscribe = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("/api/stripe");
+
+            window.location.href = response.data.url;
+        } catch (error) {
+            console.log(error, "STRIPE_CLIENT_ERROR");
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
             <DialogContent>
@@ -39,7 +55,7 @@ const ProModal = () => {
                     ))}
 
                     <DialogFooter>
-                        <Button size="lg" variant="premium" className="w-full">
+                        <Button size="lg" variant="premium" className="w-full" onClick={onSubscribe}>
                             Upgrade <LuZap className="fill-white w-4 h-4 ml-2" />
                         </Button>
                     </DialogFooter>
